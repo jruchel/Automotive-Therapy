ac5963c81245f5e698c60de4d925536a6793fade
jruchel <jruchel254@gmail.com>
2020-12-29 20:49:58 +0100

    Added repositories missing from git

--- src/main/java/org/jruchel/carworkshop/security/SecurityConfig.java
+++ src/main/java/org/jruchel/carworkshop/security/SecurityConfig.java
@@ -1,7 +1,5 @@
 package org.jruchel.carworkshop.security;
 
-import org.jruchel.carworkshop.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
